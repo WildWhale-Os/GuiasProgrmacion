d2e006d01b5772885f8418a32981bc5cca98a253
Tomas Bravo <tbravo2019@udec.cl>
2020-05-10 16:11:28 -0400

    primer Intento Ejercicio 12, modificacion Gitignore

--- guia2/ej12.c
+++ guia2/ej12.c
@@ -2,8 +2,29 @@
 
 
 
-Timus 787. Turn for MEGA
+Timus 1787. Turn for MEGA
 timus_ID = TomasBravo
 Judge_ID = 295450ZH
 
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, k, autosRestantes, autos = 0;
+    int carsAtTime[101];
+    for (int i = 0; i < 101; i++)
+        carsAtTime[i] = 0;
+
+    scanf("%d %d", &k, &n);
+
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &carsAtTime[i]);
+
+    for (int i = 0; i <= n; i++)
+        autos += carsAtTime[i];
+
+    autosRestantes = autos - (n * k);
+    printf("%d\n", autosRestantes);
+}
