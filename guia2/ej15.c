c39f27c3f95d13ea4396ea1533f0db103fd8d8b8
Tomas Bravo <tbravo2019@udec.cl>
2020-05-11 18:48:34 -0400

    inicio ej15

--- guia2/ej15.c
+++ guia2/ej15.c
@@ -1,7 +1,22 @@
 /*
 
-Timus 1880 Psych Up's Eigenvalues
+Autor: Tomas Bravo
+
 timus_ID = TomasBravo
 Judge_ID = 295450ZH
 
+Problrma 15:
+Timus 1880 Psych Up's Eigenvalues
+
+
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+
+    return 0;
+}
